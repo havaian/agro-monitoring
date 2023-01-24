74340e663f7104959f924cce97437d3b99e32512
jurody <kamilov.2000@gmail.com>
2023-01-24 12:09:25 +0500

    pizdec script ya pitonshik ejji

--- script.py
+++ script.py
@@ -14,50 +14,60 @@ def write_static_html(path):
     with open(path, 'w', encoding='utf-8') as page_f:
         page_f.write(st_page_html)
 
-def write_route(route_name:str):
+def write_route(route_name:str, last_obj_name):
     with open("routes.json", 'a', encoding='utf-8') as route_f:
         routes = {
             'path': '/' + route_name,
             'name': route_name.capitalize(),
-            'component': route_name.capitalize() + "Component"
+            'component': last_obj_name.capitalize() + "Component"
         }
         route_f.write(json.dumps(routes))
-write_static_html(r'src\components\pages\asd.vue')
+
+def pizdec(line):
+    obj_name = line.split(":")[0].strip()
+    obj_name = line.split(":")[0].strip() if obj_name != 'key' else line.split(":")[1].strip().replace("\'", "").replace(",","")
+    if obj_name == "farmer_regions_component":
+        print('asd')
+    list_of_pages = [page.replace('_component.vue', '') for page in os.listdir(page_path)]
+    if obj_name not in list_of_pages:
+        print(obj_name)
+        new_page_path = os.path.join(page_path, obj_name + '_component.vue')
+        write_static_html(new_page_path)
+    return obj_name
+
 with open(components_path, 'r', encoding='utf8') as f:
     for line in f.readlines():
-        if nl_child and line.strip() == '}\n':
+        if nl_child and line.strip() in ['}', '}\n']:
             nl_child = False
             continue
-        if len(line.split(":")) == 2 and line.split(":")[0].strip() == 'children' and general:
+        if general and len(line.split(":")) == 2 and line.split(":")[0].strip() == 'children':
             nl_child = True
             continue
         if nl_child:
-            if len(line.split(":")) == 2 and line.split(":")[1].strip() == '{':
+            if len(line.split(":")) == 2 and line.split(":")[1].strip() in ['{','{\n']:
                 route_name = line.split(":")[0].strip()
-                write_route(route_name)
+                write_route(route_name, last_obj_name)
                 continue
         if nl_obj:
-            obj_name = line.split(":")[0].strip()
-            for page in os.listdir(page_path):
-                if not page.startswith(obj_name):
-                    print(obj_name)
-                    new_page_path = os.path.join(page_path, obj_name + '_component.vue')
-                    write_static_html(new_page_path)
+            obj_name = pizdec(line)
             nl_obj = False
+            last_obj_name = obj_name
             continue
-        if line.strip() == "menuItems: {":
+        if line.strip() in ["menuItems: {", "menuItems: {\n"]:
             general = True
             nl_obj = True
             print("found")
             continue
-        if line.strip == '},\n':
+        if line.strip() in ['},\n', '},']:
             continue
         # print(line.split(": "))
-        if general and len(line.split(":")) == 2 and line.split(":")[1].strip() == '{':
+        if general and len(line.split(":")) == 2 and line.split(":")[1].strip() in ['{', '{\n']:
             print("*"*10)
-            nl_obj = True
+            obj_name = pizdec(line)
+            nl_obj = False
+            last_obj_name = obj_name
             continue
-        if line.strip() == "};\n":
+        if line.strip() in ["};\n", "};"]:
             break
         
     
